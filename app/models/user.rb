53221e9ebcfd1b9216c6baa8b5c7cfd4a0cdd2c5
Jack Tilly <jackftilly@gmail.com>
2016-08-30 09:30:59 -0700

    Added User authentication model level, along with configured routes and set up working StaticPages root file that shows a placeholder header

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,42 @@
 #
 
 class User < ApplicationRecord
+  validates :email, :username, :full_name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+  after_initialize :generate_session_token
+
+  def self.find_by_email_cred(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.find_by_username_cred(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+    nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(32)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def generate_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(32)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: JSON} do
+    resources :images
+    resources :comments
+    resource :likes, only: [:new, :destroy]
+    resource :user, only: [:new, :create]
+    resource :session, only: [:new, :create, :destroy]
+  end
+
+  root to: "static_pages#root"
 end
