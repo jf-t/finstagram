e5876a5fcda86f609ff95627ff1d93f1308f12a1
Jack Tilly <jackftilly@gmail.com>
2016-09-09 11:26:28 -0700

    attempt5

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,9 +10,9 @@ module.exports = {
   },
   plugins:[
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
+      "process.env": {
+         NODE_ENV: JSON.stringify("production")
+       }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress:{
