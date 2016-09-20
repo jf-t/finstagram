e387f1c679c83065d559da183c23b7d3ff5606be
Jack Tilly <jackftilly@gmail.com>
2016-09-20 15:42:40 -0700

    attempted to remove minification warnings once again

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,10 +7,10 @@ module.exports = {
     path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
-  plugins:[
+  plugins: [
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
